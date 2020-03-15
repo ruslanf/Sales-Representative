865a46c8fcc53f5cc7aa3a118a2f6fd0c2a49169
Ruslan <blackzerg@gmail.com>
2020-03-16 00:22:54 +0300

    Added profile layout

--- app/src/main/java/studio/bz_soft/freightforwarder/ui/profile/ProfileFragment.kt
+++ app/src/main/java/studio/bz_soft/freightforwarder/ui/profile/ProfileFragment.kt
@@ -1,6 +1,28 @@
 package studio.bz_soft.freightforwarder.ui.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import studio.bz_soft.freightforwarder.R
 
 class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_profile, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.apply {
+
+        }
+    }
+
+    companion object {
+        fun instance() = ProfileFragment()
+    }
 }
