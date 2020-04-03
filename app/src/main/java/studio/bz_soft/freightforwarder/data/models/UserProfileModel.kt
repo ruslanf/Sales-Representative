bbd4585a37f518a4084a84ab250ed8567a01440c
Ruslan <blackzerg@gmail.com>
2020-04-03 19:58:44 +0300

    Updated structure of UserModel

--- app/src/main/java/studio/bz_soft/freightforwarder/data/models/UserProfileModel.kt
+++ app/src/main/java/studio/bz_soft/freightforwarder/data/models/UserProfileModel.kt
@@ -6,12 +6,11 @@ data class UserProfileModel(
     @SerializedName("user_id") val userId: Int,
     @SerializedName("email") val email: String?,
     @SerializedName("first_name") val firstName: String?,
-    @SerializedName("middle_name") val middleName: String?,
     @SerializedName("last_name") val lastName: String?,
-    @SerializedName("phone") val phone: String?,
-    @SerializedName("photo") val photo: String?,
+    @SerializedName("s_name") val middleName: String?,
     @SerializedName("birthday") val birthday: String?,
-    @SerializedName("country") val country: String?,
+    @SerializedName("gender") val photo: String?,
+    @SerializedName("phone") val phone: String?,
     @SerializedName("city") val city: String?,
     @SerializedName("manager") val manager: String?
 )
