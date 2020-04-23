f3a3eeca4552e8ec190a72d3a170a672647352a4
Ruslan <blackzerg@gmail.com>
2020-04-23 23:57:35 +0300

    Added RecyclerView for show list of trade points

--- app/src/main/java/studio/bz_soft/freightforwarder/ui/stores/StoresFragment.kt
+++ app/src/main/java/studio/bz_soft/freightforwarder/ui/stores/StoresFragment.kt
@@ -1,32 +1,50 @@
 package studio.bz_soft.freightforwarder.ui.stores
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_root.*
 import kotlinx.android.synthetic.main.fragment_stores.view.*
 import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
 import studio.bz_soft.freightforwarder.R
+import studio.bz_soft.freightforwarder.data.http.Left
+import studio.bz_soft.freightforwarder.data.http.Right
+import studio.bz_soft.freightforwarder.data.models.StorePointModel
 import studio.bz_soft.freightforwarder.data.models.db.Location
+import studio.bz_soft.freightforwarder.root.*
+import studio.bz_soft.freightforwarder.root.Constants.EMPTY_STRING
 import studio.bz_soft.freightforwarder.root.Constants.KEY_LATITUDE
 import studio.bz_soft.freightforwarder.root.Constants.KEY_LONGITUDE
 import studio.bz_soft.freightforwarder.root.Constants.KEY_WORK_STARTED
-import studio.bz_soft.freightforwarder.root.formattedDate
-import studio.bz_soft.freightforwarder.root.getCurrentDT
-import studio.bz_soft.freightforwarder.root.parseDate
-import studio.bz_soft.freightforwarder.root.showToast
+import studio.bz_soft.freightforwarder.root.delegated.DelegateAdapter
+import studio.bz_soft.freightforwarder.ui.root.RootActivity
 import kotlin.coroutines.CoroutineContext
 
 class StoresFragment : Fragment(), CoroutineScope {
 
     private val presenter by inject<StoresPresenter>()
 
+    private val logTag = StoresFragment::class.java.simpleName
+
     private var job = Job()
     override val coroutineContext: CoroutineContext get() = Dispatchers.Main + job
+
+    private var token = EMPTY_STRING
+    private var storePointModel: List<StorePointModel>? = null
+    private var ex: Exception? = null
+
+    private val tradePointAdapter = DelegateAdapter(TradePointItemDelegate {})
+
+    private var recyclerViewState: Parcelable? = null
+    private var position = 0
 
     private var isWorkStarted = false
     private var location: Location? = null
@@ -39,6 +57,7 @@ class StoresFragment : Fragment(), CoroutineScope {
             isWorkStarted = it
         }
         getLastLocation()
+        presenter.getUserToken()?.let { token = it }
     }
 
     override fun onCreateView(
@@ -51,9 +70,25 @@ class StoresFragment : Fragment(), CoroutineScope {
         super.onViewCreated(view, savedInstanceState)
         view.apply {
             getCurrentDate(this)
+            loadTradePoints(this)
+
+            tradePointRV.apply {
+                adapter = tradePointAdapter
+                layoutManager = LinearLayoutManager(view.context, RecyclerView.VERTICAL, false)
+                recyclerViewState?.apply {
+                    layoutManager?.onRestoreInstanceState(recyclerViewState)
+                    scrollToPosition(tradePointRV, position)
+                }
+            }
+            swipeRefresh.setOnRefreshListener { refreshListener(this) }
 
             addStoreButton.setOnClickListener { storeButtonListener(this) }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as RootActivity).mainBottomNavigationMenu.visibility = View.VISIBLE
     }
 
     private fun storeButtonListener(v: View) {
@@ -84,6 +119,42 @@ class StoresFragment : Fragment(), CoroutineScope {
                 latitude = it.latitude
                 longitude = it.longitude
             }
+        }
+    }
+
+    private fun loadTradePoints(v: View) {
+        v.apply {
+            progressBar.visibility = View.VISIBLE
+            launch {
+                val request = async(SupervisorJob(job) + Dispatchers.IO) {
+                    when (val r = presenter.getTradePointList(token)) {
+                        is Right -> { storePointModel = r.value }
+                        is Left -> { ex = r.value }
+                    }
+                }
+                request.await()
+                progressBar.visibility = View.GONE
+                ex?.let {
+                    showError(context, it, R.string.fragment_store_error_load_trade_point, logTag)
+                } ?: run {
+                    storePointModel?.let { renderTradePoint(it) }
+                }
+            }
+        }
+    }
+
+    private fun renderTradePoint(grammar: List<StorePointModel>) {
+        tradePointAdapter.apply {
+            items.clear()
+            items.addAll(grammar.map { TradePointElement.TradePointItem(it) })
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun refreshListener(v: View) {
+        v.apply {
+            loadTradePoints(this)
+            swipeRefresh.isRefreshing = false
         }
     }
 
