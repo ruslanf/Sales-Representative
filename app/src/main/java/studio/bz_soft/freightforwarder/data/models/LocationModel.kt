658da821a6d57031c19df17f05a9f1c02636d85d
Ruslan <blackzerg@gmail.com>
2020-05-17 14:59:07 +0300

    Refactoring

--- app/src/main/java/studio/bz_soft/freightforwarder/data/models/LocationModel.kt
+++ app/src/main/java/studio/bz_soft/freightforwarder/data/models/LocationModel.kt
@@ -3,8 +3,8 @@ package studio.bz_soft.freightforwarder.data.models
 import com.google.gson.annotations.SerializedName
 
 data class LocationModel(
-    @SerializedName("user_id") val userId :Int,
-    @SerializedName("work_shift") val workShift :String,
-    @SerializedName("latitude") val latitude :Double,
-    @SerializedName("longitude") val longitude :Double
+    @SerializedName("user_id") val userId: Int,
+    @SerializedName("work_shift") val workShift: String,
+    @SerializedName("latitude") val latitude: Double,
+    @SerializedName("longitude") val longitude: Double
 )
